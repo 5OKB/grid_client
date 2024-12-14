89d35c912f00c2360565f680b0a9c52933bba9aa
Constantin Plotkin <kudrikkk@yandex.ru>
2024-12-14 20:50:18 +0100

    use refresh token mechanism (#15)
    * use refresh token mechanism
    
    * sdsd
    
    * sdd

--- src/gridgs/sdk/auth/client.py
+++ src/gridgs/sdk/auth/client.py
@@ -8,12 +8,16 @@ from gridgs.sdk.entity import Token
 
 
 class Client:
+    __PRE_EXPIRATION_SECONDS = 20
+
     __open_id_client: KeycloakOpenID
     __username: str
     __password: str
     __company_id: int
     __token: Token = None
-    __token_expires_at: datetime = None
+    __token_expires_at: datetime = datetime.min
+    __refresh_token: str = ''
+    __refresh_expires_at: datetime = datetime.min
     __lock: Lock
     __logger: logging.Logger
 
@@ -27,10 +31,28 @@ class Client:
 
     def token(self) -> Token:
         with self.__lock:
-            if self.__token is None or self.__token_expires_at is None or datetime.now() >= self.__token_expires_at:
-                self.__logger.info("Requesting auth token")
+            if self.__token is None or not self.__refresh_token or datetime.now() >= self.__refresh_expires_at:
+                self.__logger.info('Requesting new auth token')
                 oauth_token = self.__open_id_client.token(username=self.__username, password=self.__password)
-                self.__token_expires_at = datetime.now() + timedelta(seconds=int(oauth_token['expires_in']))
-                self.__token = Token(username=self.__username, company_id=self.__company_id, access_token=oauth_token['access_token'])
+                self.__set_tokens_values(oauth_token)
+            elif datetime.now() >= self.__token_expires_at:
+                self.__logger.info('Refreshing auth token')
+                oauth_token = self.__open_id_client.refresh_token(refresh_token=self.__refresh_token)
+                self.__set_tokens_values(oauth_token)
 
             return self.__token
+
+    def __set_tokens_values(self, oauth_token: dict):
+        self.__token = Token(username=self.__username, company_id=self.__company_id, access_token=oauth_token['access_token'])
+        self.__token_expires_at = datetime.now() + timedelta(seconds=int(oauth_token['expires_in'])) - timedelta(seconds=self.__PRE_EXPIRATION_SECONDS)
+
+        self.__refresh_token = oauth_token['refresh_token']
+        self.__refresh_expires_at = datetime.now() + timedelta(seconds=int(oauth_token['refresh_expires_in'])) - timedelta(seconds=self.__PRE_EXPIRATION_SECONDS)
+
+        self.__logger.info('Auth token', extra=_log_with_auth_token(oauth_token))
+
+
+def _log_with_auth_token(value: dict) -> dict:
+    if isinstance(value, dict):
+        return {'oauth_expires_in': value.get('expires_in'), 'oauth_refresh_expires_at': value.get('refresh_expires_in')}
+    return {}
